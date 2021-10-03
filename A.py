04126f03f96087af2a5e6f28d631bba298ce6268
Jatin Chauhan <47472473+jatinbharti146@users.noreply.github.com>
2021-10-03 20:56:47 +0530

    new changes

--- A.py
+++ A.py
@@ -5,7 +5,10 @@ def swapList(newList):
     temp = newList[0]
     newList[0] = newList[size - 1]
     newList[size - 1] = temp
-     
+    print("some new changes has been proposed")
+    print("some new changes has been proposed")
+    print("some new changes has been proposed")
+    print("some new changes has been proposed")
     return newList
      
 # Driver code
